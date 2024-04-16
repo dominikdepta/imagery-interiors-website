4280c644fd56b2dfaad428fef19bfc630f2a0467
Dominik Depta <dominikdepta@gmail.com>
2024-04-16 12:52:29 +0200

    update tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,21 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
+		fontFamily: {
+			serif: ['Spectral', 'serif'],
+			sans: ['Arial', 'sans']
+		},
+		colors: {
+			black: '#121110',
+			lightBlack: '#33302A',
+			blue: '#93ABBE',
+			white: '#EFEEEE',
+			lightGray: '#E1E1E0',
+			mediumGray: '#D9D9D9',
+			darkGray: '#909090',
+			brown: '#BF886D',
+			green: '#9DA27E'
+		}
 	},
 	plugins: [],
 }
