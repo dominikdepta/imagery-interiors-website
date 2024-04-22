98d9ac18503b615626980148ea30ea7c2707ad34
Dominik Depta <dominikdepta@gmail.com>
2024-04-22 11:52:07 +0200

    add FooterNewsletter wrapper

---
+++ src/modules/NewsletterForm/NewsletterForm.tsx
@@ -0,0 +1,7 @@
+export interface NewsletterFormProps {
+  className?: string;
+}
+
+export const NewsletterForm = ({ className }: NewsletterFormProps) => {
+  return <form className={className}>[NewsletterForm]</form>;
+};
