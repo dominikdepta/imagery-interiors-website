824e08c5df4fcc15daeb98d03469c544dfbae854
Dominik Depta <dominikdepta@gmail.com>
2024-04-22 08:47:58 +0200

    update reviewsSliderNavButton styles

--- src/modules/ReviewsSection/ReviewsSliderNavButton/ReviewsSliderNavButton.tsx
+++ src/modules/ReviewsSection/ReviewsSliderNavButton/ReviewsSliderNavButton.tsx
@@ -14,11 +14,15 @@ export const ReviewsSliderNavButton = ({
   ...restProps
 }: ReviewsSliderNavButtonProps) => (
   <button
-    className={`flex flex-col gap-1 p-1 text-lg uppercase leading-none ${className}`}
+    className={`flex flex-col items-center gap-1 p-1 text-lg uppercase leading-none ${className}`}
     {...restProps}
   >
     {children}
 
-    <img src={type === "prev" ? arrowLeft.src : arrowRight.src} />
+    <img
+      src={type === "prev" ? arrowLeft.src : arrowRight.src}
+      width={47}
+      height={10}
+    />
   </button>
 );
