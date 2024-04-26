cf635ef8ad2b9418e62cc06229ba0d41d8a55e77
Dominik Depta <dominikdepta@gmail.com>
2024-04-26 15:59:49 +0200

    add depth param to isActivePath

--- src/utils/isActivePath.ts
+++ src/utils/isActivePath.ts
@@ -1,4 +1,23 @@
 import type { APIContext } from "astro";
 
-export const isActivePath = (context: APIContext, path: string) =>
-  context.url.pathname === path;
+export const remoteTrailingSlash = (path: string) => path.replace(/\/$/, "");
+
+export const isActivePath = (context: APIContext, path: string, depth = 0) => {
+  const currentPath = remoteTrailingSlash(context.url.pathname);
+  const paramPath = remoteTrailingSlash(path);
+
+  if (depth > 0) {
+    return (
+      currentPath
+        .split("/")
+        .slice(1, depth + 1)
+        .join("/") ===
+      paramPath
+        .split("/")
+        .slice(1, depth + 1)
+        .join("/")
+    );
+  }
+
+  return currentPath === paramPath;
+};
